ecb070e4de85d0e332b62da0e4eba562b63d6a96
Edgar Ong <emailedgar@gmail.com>
2019-06-25 09:40:23 -0400

    Test action cable and foursquare api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { API_SECRET } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ActionCableConsumer } from 'react-actioncable-provider'
+
+class App extends React.Component {
+  state = {
+    venues: [],
+    on: false
+  }
+
+  componentDidMount() {
+    let getLoc = () => {
+      navigator.geolocation.getCurrentPosition(position => {
+        localStorage.setItem('lat', position.coords.latitude)
+        localStorage.setItem('long', position.coords.longitude)
+      })
+    }
+
+    getLoc()
+
+    if (localStorage.lat && localStorage.long && this.state.on) {
+      fetch(`https://api.foursquare.com/v2/venues/explore?section=food&ll=${localStorage.getItem('lat')},${localStorage.getItem('long')}&client_id=${API_SECRET.id}&client_secret=${API_SECRET.secret}&v=${API_SECRET.version}`)
+        .then(r => r.json())
+        .then(data => {
+          this.setState({
+            venues: data.response.groups[0].items
+          })
+        })
+    }
+  }
+
+  pressMe = () => {
+    // ActionCable
+    return fetch('http://localhost:8000/users')
+      .then(r => r.json())
+  }
+
+  render() {
+    console.log('App state', this.state)
+    return (
+      <div>
+        Hello from App.
+        <br />
+        Venues: {this.state.venues.length}
+        <br />
+        <button onClick={() => this.pressMe()}>Press it</button>
+        <ActionCableConsumer
+          channel={{ channel: "ChatThreadChannel" }}
+          onReceived={users => console.log(users)} />
+      </div>
+    );
+  }
 }
 
 export default App;
