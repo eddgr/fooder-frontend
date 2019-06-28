7af7087a5dfcad318df00fe98e897fb415128e8d
Edgar Ong <emailedgar@gmail.com>
2019-06-28 09:24:20 -0400

    send location data to backend

--- src/components/LikeVenue.js
+++ src/components/LikeVenue.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function LikeVenue(props) {
-  console.log(props)
+
   return (
     <div className="card m-4">
       <div className="row no-gutters">
